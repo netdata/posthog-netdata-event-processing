c6956056310d26684543bfb00d8dbf33eafa1639
Andrew Maguire <andrewm4894@gmail.com>
2021-03-18 15:28:11 +0000

    add logic for data_testid

--- index.js
+++ index.js
@@ -104,6 +104,8 @@ async function processEvent(event, { config, cache }) {
     // extract useful properties from elements
     if (event.elements) {
 
+        event.properties['elements_copy'] = event.elements
+
         // loop over each element
         event.elements.forEach((element) => {
 
