bdf08a708197d7bd923cd4a078bbc107c3af3589
Andrew Maguire <andrewm4894@gmail.com>
2021-07-15 21:28:59 +0100

    add cloud processing

--- process.js
+++ process.js
@@ -1,5 +1,6 @@
 import { processElementsAgent } from './process_elements_agent';
 import { processPropertiesAgent } from './process_properties_agent';
+import { processElementsCloud } from './process_elements_cloud';
 
 async function setupPlugin({ config, global }) {
     console.log("Setting up the plugin!")
@@ -17,6 +18,7 @@ async function processEvent(event, { config, cache }) {
 
             if (event.properties['$current_url'].startsWith('https://app.netdata.cloud') ) {
                 event.properties['event_source'] = 'cloud'
+                event = processElementsCloud(event)
             } else if (event.properties['$current_url'].startsWith('https://www.netdata.cloud') ) {
                 event.properties['event_source'] = 'website'
             } else if (event.properties['$current_url'].startsWith('https://learn.netdata.cloud') ) {
