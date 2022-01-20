41a42e0526082508dd903ea46cfeec15267a3ca7
Andrew Maguire <andrewm4894@gmail.com>
2022-01-20 21:52:53 +0000

    add handling for a lone flag at end of options

--- __tests__/index_agent_installer.js
+++ __tests__/index_agent_installer.js
@@ -37,7 +37,7 @@ test('install_options_easy', async () => {
     const event = createEvent({
         event: 'test event', properties: {
             "$current_url":"agent installer",
-            "install_options": "--dont-start-it --dont-wait --claim-token 3gciBd6HYGp7Z2v2fJDd6meraFoUT4QpVYcHTE253KujJPStNQhXi9cicTgEEc_mNiQNxAYtHlZNpC1a2NQz57fV6aZaa2vPvyPYw9hsv_SOfzfWxMdQ6L-PPOyM9e9N2HAVp7E --claim-rooms 22ff1e07-8e9c-41ad-b141-5bd95fbf95d1 --claim-url https://app.netdata.cloud"
+            "install_options": "--dont-start-it --dont-wait --claim-token 3gciBd6HYGp7Z2v2fJDd6meraFoUT4QpVYcHTE253KujJPStNQhXi9cicTgEEc_mNiQNxAYtHlZNpC1a2NQz57fV6aZaa2vPvyPYw9hsv_SOfzfWxMdQ6L-PPOyM9e9N2HAVp7E --claim-rooms 22ff1e07-8e9c-41ad-b141-5bd95fbf95d1 --claim-url https://app.netdata.cloud --foo"
         }
     })
     const eventCopy = await processEvent(clone(event), getMeta())
@@ -46,6 +46,7 @@ test('install_options_easy', async () => {
     expect(eventCopy['properties']['opt_dont_wait']).toEqual('')
     expect(eventCopy['properties']['opt_claim_token']).toEqual('3gciBd6HYGp7Z2v2fJDd6meraFoUT4QpVYcHTE253KujJPStNQhXi9cicTgEEc_mNiQNxAYtHlZNpC1a2NQz57fV6aZaa2vPvyPYw9hsv_SOfzfWxMdQ6L-PPOyM9e9N2HAVp7E')
     expect(eventCopy['properties']['opt_claim_url']).toEqual('https://app.netdata.cloud')
+    expect(eventCopy['properties']['opt_foo']).toEqual('')
 })
 
 
